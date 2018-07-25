6fca275ec6f0026dd9fc6e36f7b5b518e9bbcd50
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-07-25 16:36:01 -0400

    updated jest test to be fully async

--- test/s3.test.js
+++ test/s3.test.js
@@ -12,50 +12,36 @@ const s3 = require('../lib/s3')(new AWS.S3(), {
 })
 
 describe('Basic S3 Operations', () => {
-  it ('should upload and get a file from S3', done => {
-    s3.put("test.json", JSON.stringify({ "hello": "world" }))
-    .then((data) => {
-      expect(data).toMatchObject({
-        ETag: expect.anything()
-      })
-      return s3.get("test.json")
+  it ('should upload and get a file from S3', async () => {
+    var data = await s3.put("test.json", JSON.stringify({ "hello": "world" }))
+    expect(data).toMatchObject({
+      ETag: expect.anything()
     })
-    .then((data) => {
-      var json = JSON.parse(data)
-      expect(json).toMatchObject({
-        hello: 'world'
-      })
-      return s3.copy("test.json", "test2.json")
+    data = await s3.get("test.json")
+    var json = JSON.parse(data)
+    expect(json).toMatchObject({
+      hello: 'world'
     })
-    .then((data) => {
-      console.log(data)
-      expect(data).toHaveProperty('CopyObjectResult.ETag')
-      return s3.list("test")
-    })
-    .then((data) => {
-      expect(data).toHaveProperty('KeyCount', 2)
-      expect(data).toHaveProperty('Contents')
-      expect(data.Contents).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            Key: "test.json",
-          }),
-          expect.objectContaining({
-            Key: "test2.json"
-          })
-        ])
-      )
-      return s3.delete(data.keys)
-    })
-    .then((data) => {
-      console.log(data)
-      expect(data).toHaveProperty('keys')
-      expect(data.keys).toEqual(
-        expect.arrayContaining([ "test.json", "test2.json" ])
-      )
-      done()
-    })
-  })
+    data = await s3.copy("test.json", "test2.json")
+    expect(data).toHaveProperty('CopyObjectResult.ETag')
+    data = await s3.list("test")
+    expect(data).toHaveProperty('KeyCount', 2)
+    expect(data).toHaveProperty('Contents')
+    expect(data.Contents).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          Key: "test.json",
+        }),
+        expect.objectContaining({
+          Key: "test2.json"
+        })
+      ])
+    )
+    data = await s3.delete(data.keys)
+    expect(data).toHaveProperty('keys')
+    console.log(data.keys)
+    expect(data.keys).toEqual([ "test.json", "test2.json" ])
+  }, 30000)
   it ('should get a presigned url to put object', async () => {
     var signed = await s3.getSignedPutUrl("test.json", { 'ContentType': 'application/json' })
     console.log(signed)
@@ -65,9 +51,13 @@ describe('Basic S3 Operations', () => {
   it ('should upload a presigned url for a zip object (non-streaming)', async () => {
     var path = `${__dirname}/index.test.zip`
     var size = fs.statSync(path).size
-    var signed = await s3.getSignedPutUrl('index.test.zip', { 
+    // var signed = await s3.getSignedPutUrl('index.test.zip', { 
+    //   'ContentType': 'application/zip'
+    // })
+    var signed = await s3.getSignedPutUrl('users/0edb3176-e3d9-45ac-8a26-b6d43cb1f6d1/functions/a2a3a9d4-a1f2-47d3-b123-72e47cff933e/index.test.zip', { 
       'ContentType': 'application/zip'
     })
+
     var res = (await axios.put(signed.url, 
       //fs.createReadStream(path), { 
         fs.readFileSync(path), { 
